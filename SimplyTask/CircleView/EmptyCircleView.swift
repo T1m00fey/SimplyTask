5e61a7c8a44f5f95bf155ce08c26e3ed1cbf85f6
T1m00fey <forG1tHub@yandex.ru>
2023-12-07 16:31:50 +0400

    Updates

---
+++ SimplyTask/CircleView/EmptyCircleView.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyCircleView.swift
+//  SimplyTask
+//
+//  Created by Тимофей Юдин on 07.12.2023.
+//
+
+import SwiftUI
+
+struct EmptyCircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyCircleView()
+}
