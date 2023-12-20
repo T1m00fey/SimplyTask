c4b1e2017e9ea5a244a1af613cf07129b2377a9a
T1m00fey <forG1tHub@yandex.ru>
2023-12-20 15:45:50 +0400

    updates

---
+++ SimplyTask/TasksList/ListsView.swift
@@ -0,0 +1,18 @@
+//
+//  ListsView.swift
+//  SimplyTask
+//
+//  Created by Тимофей Юдин on 20.12.2023.
+//
+
+import SwiftUI
+
+struct ListsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListsView()
+}
